d14387ea32a77902480c96c47aa3acd52ae7e689
Farhan Ahmad <farhanhubble@gmail.com>
2020-04-16 19:32:55 +0530

    Disable logging to file to circumvent serialization error.

--- deepdrive_zero/logs.py
+++ deepdrive_zero/logs.py
@@ -2,7 +2,7 @@ from loguru import logger as log
 from os.path import dirname, realpath, join
 import os
 
-ROOT_DIR = dirname(realpath(__file__))
-LOG_DIR = join(ROOT_DIR, 'logs')
-os.makedirs(LOG_DIR, exist_ok=True)
-log.add(join(LOG_DIR, 'deepdrive-2d-log-{time}.log'))
+# ROOT_DIR = dirname(realpath(__file__))
+# LOG_DIR = join(ROOT_DIR, 'logs')
+# os.makedirs(LOG_DIR, exist_ok=True)
+# log.add(join(LOG_DIR, 'deepdrive-2d-log-{time}.log'))
